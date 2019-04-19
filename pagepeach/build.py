bb77d27638239a499a7315c4860dda86533456b4
Lucas Hild <contact@lucas-hild.de>
2019-04-19 18:18:11 +0200

    Add template rendering

--- pagepeach/build.py
+++ pagepeach/build.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 from pathlib import Path
+
+from template import Page
 
 
 class Build:
@@ -7,13 +10,34 @@ class Build:
         self.config = config
 
     def build(self, docs_path):
+        dist_path = Path("dist")
+        self.prepare_dist(dist_path)
+
         for root, dirs, files in os.walk(docs_path):
             for name in files:
                 filepath = Path(root, name)
-                if filepath.suffix == ".md":
-                    file = File(filepath)
-                    file_content = file.content()
-                    print(file_content)
+                if filepath.suffix != ".md":
+                    continue
+
+                file = File(filepath)
+                file_content = file.content()
+
+                filename = name.replace(".md", ".html")
+                page = Page(self.config, file_content)
+                html = page.generate_html()
+                self.save_html(filename, html, dist_path)
+
+    def prepare_dist(self, dist_path):
+        template_path = Path(os.path.realpath(__file__)).parent / "template"
+
+        shutil.rmtree(dist_path)
+        dist_path.mkdir(parents=True, exist_ok=True)
+
+        shutil.copyfile(template_path / "style.css", dist_path / "style.css")
+
+    def save_html(self, name, html, dist_path):
+        with open(dist_path / name, "w") as f:
+            f.write(html)
 
 
 class File:
