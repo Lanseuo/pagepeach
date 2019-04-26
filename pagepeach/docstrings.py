6badb0a83e829a42770adc1d8118e48dc3f772c4
Lucas Hild <contact@lucas-hild.de>
2019-04-26 20:59:52 +0200

    Add classes for functions and classes in docstrings

--- pagepeach/docstrings.py
+++ pagepeach/docstrings.py
@@ -4,19 +4,27 @@ from pathlib import Path
 FILENAME = Path("script.py")
 
 
+class Function:
+    def __init__(self, node):
+        self.node = node
+
+
+class Class:
+    def __init__(self, node):
+        self.node = node
+        self.methods = self.get_methods()
+
+    def get_methods(self):
+        return [Function(subnode) for subnode in self.node.body if isinstance(subnode, ast.FunctionDef)]
+
+
 def get_docstrings():
     with open(FILENAME, "r") as f:
         file_content = f.read()
-
     module = ast.parse(file_content)
-    functions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
 
-    for function in functions:
-        print("---")
-        print(str(FILENAME) + "::" + function.name)
-        print(ast.get_docstring(function))
-
-    print("---")
+    functions = [Function(node) for node in module.body if isinstance(node, ast.FunctionDef)]
+    classes = [Class(node) for node in module.body if isinstance(node, ast.ClassDef)]
 
 
 get_docstrings()
